7a90b175a609e516390a073f4ac56f9f4cc16a29
AshwinM <ashwinmohanan008@gmail.com>
2024-08-15 18:55:22 -0500

    Started working on React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Y</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
